dedb9f6247cd9a08e888d0230481ab0a399cfd42
Atsushi Mizumoto <atsushi@mizumot.com>
2015-08-19 03:12:54 +0900

    update

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ ctt
 ===
 
 Classical Test Theory (Item Analysis)
+
+
+### About
+You can check out this application here:
+- http://langtest.jp/shiny/ctt/
+
+### Author
+[Atsushi MIZUMOTO](http://mizumot.com/ "mizumot.com"), Ph.D.   
+Associate Professor of Applied Linguistics  
+Faculty of Foreign Language Studies, Kansai University, Osaka, Japan
--- ui.R
+++ ui.R
@@ -120,7 +120,7 @@ shinyUI(bootstrapPage(
             code('library(psych)'),br(),
             code('library(ltm)'),br(),
             code('library(CTT)'),br(),
-
+            code('library(beeswarm)'),br(),
             br(),
 
             strong('Code'),
@@ -138,13 +138,23 @@ shinyUI(bootstrapPage(
 
             br(),
 
+            strong('Citation in Publications'),
+            p('Mizumoto, A. (2015). Langtest (Version 1.0) [Web application]. Retrieved from http://langtest.jp'),
+
+            br(),
+
+            strong('Article'),
+            p('Mizumoto, A., & Plonsky, L. (2015).', a("R as a lingua franca: Advantages of using R for quantitative research in applied linguistics.", href='http://applij.oxfordjournals.org/content/early/2015/06/24/applin.amv025.abstract', target="_blank"), em('Applied Linguistics,'), 'Advance online publication. doi:10.1093/applin/amv025'),
+
+            br(),
+
             strong('Recommended'),
             p('To learn more about R, I suggest this excellent and free e-book (pdf),',
             a("A Guide to Doing Statistics in Second Language Research Using R,", href="http://cw.routledge.com/textbooks/9780805861853/guide-to-R.asp", target="_blank"),
             'written by Dr. Jenifer Larson-Hall.'),
 
             p('Also, if you are a cool Mac user and want to use R with GUI,',
-            a("MacR", href="http://www.urano-ken.com/blog/2013/02/25/installing-and-using-macr/", target="_blank"),
+            a("MacR", href="https://sites.google.com/site/casualmacr/", target="_blank"),
             'is defenitely the way to go!'),
 
             br(),
